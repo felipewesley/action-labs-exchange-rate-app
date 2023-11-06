df5e35658fd38823d791d7aa211e051135e4cc98
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-06 20:32:22 -0300

    arquivos de environment - variáveis de ambiente da aplicação

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+	production: true,
+
+	api: {
+		baseUrl							: 'https://api-brl-exchange.actionlabs.com.br/api/1.0',
+		currentExchange			: '/open/currentExchange',
+		dailyExchangeRate		: '/open/dailyExchangeRate',
+	},
+};
