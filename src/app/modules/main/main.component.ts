0aef38c9995074be0707183dbfdd3c7f5538fdf2
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-06 21:08:47 -0300

    main service - ações da tela principal

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
+import { MainService } from "./services/main.service";
+
 @Component({
 	selector: 'app-main',
 	templateUrl: './main.component.html',
-	standalone: true
+	standalone: true,
+	providers: [
+		MainService
+	]
 })
 export class MainComponent implements OnInit {
 
---
+++ src/app/modules/main/services/main.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { environment } from "environments/environment";
+
+import { BehaviorSubject, Observable, map, take } from "rxjs";
+
+import { DailyExchangeRateModel } from "app/domain/models/daily-exchange-rate.model";
+import { CurrentExchangeRateModel } from "app/domain/models/current-exchange-rate.model";
+
+@Injectable()
+export class MainService {
+
+	private readonly _currentExchangeRate = new BehaviorSubject<CurrentExchangeRateModel>(null);
+	private readonly _dailyExchangeRate = new BehaviorSubject<DailyExchangeRateModel>(null);
+
+	public readonly currentExchangeRate = this._currentExchangeRate.asObservable();
+	public readonly dailyExchangeRate = this._dailyExchangeRate.asObservable();
+
+	constructor(
+		private _http: HttpClient
+	) { }
+
+	/**
+	 * Fetches the current exchange rate from the api and updates its subject
+	 * @param fromSymbol
+	 * @param toSymbol
+	 */
+	public fetchCurrentExchangeRate(fromSymbol: string, toSymbol: string): Observable<void>;
+	public fetchCurrentExchangeRate(fromSymbol: string, toSymbol: string): Observable<void> {
+
+		return this._fetchCurrentExchangeRate(fromSymbol, toSymbol)
+			.pipe(
+				map(currentExchangeRate => {
+					this._currentExchangeRate.next(currentExchangeRate);
+				})
+			);
+	}
+
+	/**
+	 * Fetches the daily exchange rate from the api and updates its subject
+	 * @param fromSymbol
+	 * @param toSymbol
+	 */
+	public fetchDailyExchangeRate(fromSymbol: string, toSymbol: string): Observable<void>;
+	public fetchDailyExchangeRate(fromSymbol: string, toSymbol: string): Observable<void> {
+
+		return this._fetchDailyExchangeRate(fromSymbol, toSymbol)
+			.pipe(
+				map(dailyExchangeRate => {
+					this._dailyExchangeRate.next(dailyExchangeRate);
+				})
+			);
+	}
+
+	private _fetchCurrentExchangeRate(fromSymbol: string, toSymbol: string): Observable<CurrentExchangeRateModel> {
+
+		const url = `${environment.api.baseUrl}${environment.api.currentExchangeRate}`;
+
+		const params = {
+			from_symbol: fromSymbol,
+			to_symbol: toSymbol
+		};
+
+		return this._http.get<CurrentExchangeRateModel>(url, { params })
+			.pipe(
+				take(1)
+			);
+	}
+
+	private _fetchDailyExchangeRate(fromSymbol: string, toSymbol: string): Observable<DailyExchangeRateModel> {
+
+		const url = `${environment.api.baseUrl}${environment.api.dailyExchangeRate}`;
+
+		const params = {
+			from_symbol: fromSymbol,
+			to_symbol: toSymbol
+		};
+
+		return this._http.get<DailyExchangeRateModel>(url, { params })
+			.pipe(
+				take(1)
+			);
+	}
+}
