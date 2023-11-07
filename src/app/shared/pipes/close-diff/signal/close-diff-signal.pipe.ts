f3486e2774fb72773502f091a336481a84fdd118
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-07 16:51:31 -0300

    remoção log de teste

--- src/app/shared/pipes/close-diff/signal/close-diff-signal.pipe.ts
+++ src/app/shared/pipes/close-diff/signal/close-diff-signal.pipe.ts
@@ -11,8 +11,6 @@ export class CloseDiffSignalPipe implements PipeTransform {
 
 	transform(value: number): string {
 
-		console.log('signal =>', value);
-
 		if (value == 0) {
 			return value.toString();
 		}
