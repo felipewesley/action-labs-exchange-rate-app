38441d225f92153136c644b56b99fb03a11b8678
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-07 01:01:30 -0300

    current exchange rate component layout

---
+++ src/app/modules/main/components/current-exchange-rate/current-exchange-rate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: 'app-main-current-exchange-rate',
+	templateUrl: './current-exchange-rate.component.html',
+	standalone: true,
+})
+export class MainCurrentExchangeRateComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit(): void {
+
+	}
+}
