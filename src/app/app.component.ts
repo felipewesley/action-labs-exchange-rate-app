dad32fb418c43da92e82bd4940c09d39425585ed
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-07 00:44:11 -0300

    layout component carregado na tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LayoutComponent } from './layout/layout.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 	standalone: true,
 	imports: [
-		RouterOutlet
+		RouterOutlet,
+		LayoutComponent
 	]
 })
 export class AppComponent {
