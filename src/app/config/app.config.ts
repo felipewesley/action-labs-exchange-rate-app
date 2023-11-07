c5c0be6f5ae196501cd8b70eddeb7241ec735e9d
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-07 00:19:50 -0300

    app config - constant

---
+++ src/app/config/app.config.ts
@@ -0,0 +1,16 @@
+/**
+ * @type
+ */
+export type AppConfig = {
+	appTitle: string;
+	companyName: string;
+};
+
+/**
+ * Application config
+ * @constant appConfig
+ */
+export const appConfig: AppConfig = {
+	appTitle: 'BRL Exchange Rate',
+	companyName: 'Action Labs',
+};
