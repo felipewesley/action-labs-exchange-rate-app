33c774f54a32a807322737db7f59b25eb6e6befe
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-07 01:34:40 -0300

    tailwind config - primary theme color e font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,scss,ts}'],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				primary: '#07B0FB'
+			},
+		},
+		fontFamily: {
+			body: ['Roboto', '"Helvetica Neue"', ...defaultTheme.fontFamily.sans],
+		},
   },
   plugins: [],
 }
