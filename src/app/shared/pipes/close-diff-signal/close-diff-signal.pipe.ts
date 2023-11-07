2d5f3ab729d0ebc326fc9d1e91a0cdee6fa41097
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-07 15:14:43 -0300

    close diff signal pipe

---
+++ src/app/shared/pipes/close-diff-signal/close-diff-signal.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+	name: 'closeDiffSignal',
+	pure: true,
+	standalone: true,
+})
+export class CloseDiffSignalPipe implements PipeTransform {
+
+	constructor() { }
+
+	transform(value: number): string {
+
+		if (value == 0) {
+			return value.toString();
+		}
+
+		const signal = value > 0 ? '+' : '-';
+
+		return `${signal}${value}`;
+	}
+}
