9898bfa4dfd59621aab7783e7a62c5cbe1239657
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-06 22:02:21 -0300

    fluxo inicial do component principal do app

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { Subject, filter, map, switchMap, takeUntil, tap } from "rxjs";
 
 import { MainService } from "./services/main.service";
 
@@ -10,11 +14,61 @@ import { MainService } from "./services/main.service";
 		MainService
 	]
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
-	constructor() { }
+	public readonly currencyCodeControl = new FormControl<string>(null);
+
+	private readonly _unsubscribeAll = new Subject<void>();
+
+	constructor(
+		private _service: MainService,
+		private _router: Router,
+		private _activatedRoute: ActivatedRoute
+	) { }
 
 	ngOnInit(): void {
 
+		this._activatedRoute.queryParamMap
+			.pipe(
+				map(params => params.get('symbol')),
+				tap(symbol => {
+					if (symbol == null) {
+						this._service.clearCurrentExchangeRate();
+					}
+				}),
+				filter(symbol => symbol != null),
+				switchMap(symbol => this._service.fetchCurrentExchangeRate('BRL', symbol)),
+				takeUntil(this._unsubscribeAll)
+			)
+			.subscribe();
+	}
+
+	ngOnDestroy(): void {
+		this._unsubscribeAll.next();
+		this._unsubscribeAll.complete();
+	}
+
+	public fetchResult(): void {
+
+		const fromSymbol = this.currencyCodeControl.value;
+
+		// Validations
+
+		this._router.navigate(['.'], {
+			queryParams: {
+				symbol: fromSymbol
+			}
+		});
+	}
+
+	public fetchDailyResults(): void {
+
+		const fromSymbol = this.currencyCodeControl.value;
+		const toSymbol = 'BRL';
+
+		// Validations
+
+		this._service.fetchDailyExchangeRate(fromSymbol, toSymbol)
+			.subscribe();
 	}
 }
