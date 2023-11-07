3201d18768487445c556e3900aca2f7eae357ecc
Felipe Basso <fe.wesleybasso@gmail.com>
2023-11-06 21:10:53 -0300

    provide browser animations module - dependência

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { bootstrapApplication } from "@angular/platform-browser";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideRouter } from "@angular/router";
 
 import { AppComponent } from "app/app.component";
@@ -9,7 +10,12 @@ bootstrapApplication(AppComponent, {
 		/**
 		 * App routes
 		 */
-		provideRouter(appRoutes)
+		provideRouter(appRoutes),
+
+		/**
+		 * Browser animation dependencies
+		 */
+		provideAnimations(),
 	]
 })
 .catch(err => console.error(err));
